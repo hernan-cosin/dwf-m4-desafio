d7d8c031176e8b4dfb25ca494b1f4100e0c38f91
hernan-cosin <78047711+hernan-cosin@users.noreply.github.com>
2021-07-07 11:53:03 -0300

    Form contact submit POST request working

--- componentes/form-contact/index.js
+++ componentes/form-contact/index.js
@@ -1,5 +1,4 @@
 function contactForm(el) {
-  //   const contactContainer = document.querySelector(".contact-container");
   const contactComponent = document.createElement("div");
   contactComponent.classList = "contact-component";
   contactComponent.innerHTML = `
@@ -8,16 +7,58 @@ function contactForm(el) {
     </h2>
     <form class="form">
         <label class="form__label" for="name">NOMBRE
-            <input type="text" class="form__input" id="name">
+            <input name="name" type="text" class="form__input" id="name">
         </label>
         <label class="form__label" for="mail">EMAIL
-            <input type="email" class="form__input" id="email">
+            <input name="email" type="email" class="form__input" id="email">
         </label>
         <label class="form__label" for="message">MENSAJE
-            <textarea id="message" class="form__input  form__area"></textarea>
+            <textarea name="message" class="form__input  form__area" id="message"></textarea>
         </label>
         <button class="form__button">Enviar</button>
     </form> 
     `;
   el.appendChild(contactComponent);
+  formPreventDefault();
+}
+
+function formPreventDefault() {
+  const formEl = document.querySelector(".form");
+  formEl.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const mensaje = formValues(formEl);
+
+    postData("https://apx-api.vercel.app/api/utils/dwf", {
+      to: "hcosin@gmail.com",
+      message: mensaje,
+    }).then((data) => {
+      console.log(data);
+    });
+
+    formEl.reset();
+  });
+}
+
+function formValues(form) {
+  var formData = new FormData(form);
+
+  const name = formData.get("name");
+  const email = formData.get("email");
+  const message = formData.get("message");
+
+  const data = `
+    "nombre": ${name}, "email": ${email}, "mensaje": ${message};
+  `;
+
+  return data;
+}
+
+async function postData(url = "", data = {}) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  return response.json();
 }
